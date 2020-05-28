65aa58e4084463ee73309c3213b9437037833fde
Shohei Kamiya <s_kamiya@ga-tech.co.jp>
2020-05-28 14:00:59 +0900

    /homeにテーブルを表示(routingの挙動を確認、Vuetifyを使ってみた)

---
+++ pages/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    this is home
+    <MainTable />
+  </div>
+</template>
+
+<script>
+import MainTable from '../components/MainTable'
+
+export default {
+  components: {
+    MainTable
+  }
+}
+</script>
